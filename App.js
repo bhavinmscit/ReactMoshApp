1b5d1202dfb6b7843a0369f109f7169e0bb09fbb
bhavinmscit <97672258+bhavinmscit@users.noreply.github.com>
2022-02-19 17:13:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -37,11 +37,48 @@ import Screen from "./app/componant/Screen";
 import InputSwitch from "./app/InputComponents/InputSwitch";
 import AppPicker from "./app/componant/AppPicker";
 import AppPickerPage from "./app/InputComponents/AppPickerPage";
+import LoginScreen from "./app/screen/LoginScreen";
+import LoginFormik from "./app/screen/LoginFormik";
+import ListingEditScreen from "./app/screen/ListingEditScreen";
+import AppImagePickerLib from "./app/Native/AppImagePickerLib";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
+  </Screen>
+);
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
-    <Screen>
-      <AppPickerPage></AppPickerPage>
-    </Screen>
+    // <Screen>
+    //   <AppPickerPage></AppPickerPage>
+    // </Screen>
+    // <LoginFormik />
+    // <ListingEditScreen />
+    // <NavigationContainer>
+    //   <StackNavigator />
+    // </NavigationContainer>
+    // <MessagesScreen />
+    <AppImagePickerLib />
   );
 }
